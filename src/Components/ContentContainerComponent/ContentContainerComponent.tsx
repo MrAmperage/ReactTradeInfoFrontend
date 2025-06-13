bc021bde327efa97a2f0610aadd4f079da685649
Sukhovoy Alexey <a.sukhovoy@dpt-pro.ru>
2025-06-13 19:47:01 +0700

    Обновление компонента отображения контента страницы

--- src/Components/ContentContainerComponent/ContentContainerComponent.tsx
+++ src/Components/ContentContainerComponent/ContentContainerComponent.tsx
@@ -1,6 +1,10 @@
 import "./ContentContainerComponent.css"
 /*Компонент для отображения текущей открытой страницы*/
 const ContentContainerComponent: React.FC = ()=>{
-    return <div className="AllSpace">Контент</div>
+    return <div className="AllSpace FlexCenter">
+        <span className="NoContentText">
+            Нет активных вкладок
+        </span>
+        </div>
 }
 export default ContentContainerComponent
